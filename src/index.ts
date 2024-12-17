c0255c8f4246e469d2d236d1b8a7f7fbde833a94
Gianmarco Ciaffardini <ciaffardini.g@gmail.com>
2024-12-17 18:49:41 +0100

    feat: tw

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { CronJob } from "cron";
 
 import { runner } from "./runner";
 
-console.log("Running cron..");
+console.log("Running cron.. 2");
 CronJob.from({
   cronTime: "0 21 * * *", // everyday at 21pm
   onTick: runner,
